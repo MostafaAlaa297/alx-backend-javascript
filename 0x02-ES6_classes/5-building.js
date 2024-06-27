d77b707d053d9daae1eab17b8ce20d2e474e1bfc
MostafaAlaa297 <elaskary609@gmail.com>
2024-06-27 16:09:36 -0700

    updwqate Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,11 @@
 class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
     this.sqft = sqft;
   }
@@ -15,10 +19,6 @@ class Building {
       throw new TypeError('Square footage must be a number');
     }
     this._sqft = value;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
 
