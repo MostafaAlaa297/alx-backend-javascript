e0713f6991012a8a275ff528eca92dc47b71857c
MostafaAlaa297 <elaskary609@gmail.com>
2024-06-27 15:53:11 -0700

    create Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+class Building {
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new TypeError('Cannot construct Building instances directly');
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Square footage must be a number');
+    }
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
